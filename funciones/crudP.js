7133ea3cde21ed3e6917dc791aea7cf8e2c42679
DarwinCharris <98895036+DarwinCharris@users.noreply.github.com>
2023-10-18 22:48:00 -0500

    eliminación

--- funciones/crudP.js
+++ funciones/crudP.js
@@ -1,3 +1,69 @@
+const btnrfs = document.getElementById('refresh');
+btnrfs.addEventListener('click',()=>{
+  cargar();
+});
+
+
+function cargar(){
+  let queri= 'select * from padre'
+  url = `http://localhost:3001/api/data?query=${encodeURIComponent(queri)}`;
+  
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error en la respuesta de la solicitud: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Limpiar la tabla antes de llenarla
+      const tableBody = document.querySelector('tbody');
+      tableBody.innerHTML = '';
+
+      // Llenar la tabla con los datos
+      data.forEach(record => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${record.id}</td>
+          <td>${record.nom}</td>
+          <td><button class="eliminar" data-id="${record.id}">Eliminar</button></td>
+        `;
+
+        tableBody.appendChild(row);
+      });
+
+      // Agregar event listener para los botones de eliminar
+      const botonesEliminar = document.querySelectorAll('.eliminar');
+      botonesEliminar.forEach(boton => {
+        boton.addEventListener('click', () => {
+          const idAEliminar = boton.getAttribute('data-id');
+          eliminarRegistro(idAEliminar);
+        });
+      });
+    })
+    .catch(error => {
+      console.error('Error al obtener datos:', error);
+    });
+}
+function eliminarRegistro(id) {
+  // Realiza una solicitud para eliminar el registro con el ID proporcionado en la base de datos
+  let queri = `DELETE FROM padre WHERE id = ${id}`;
+  url = `http://localhost:3001/api/data?query=${encodeURIComponent(queri)}`;
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error en la respuesta de la solicitud: ${response.status}`);
+      }
+      // Recargar la tabla después de eliminar el registro
+      cargar();
+    })
+    .catch(error => {
+      console.error('Error al eliminar el registro:', error);
+    });
+}
+
+
 const btninsert = document.getElementById('insertar');
 btninsert.addEventListener('click', ()=>{
     const txtid = document.getElementById('ident');
@@ -21,7 +87,9 @@ fetch(url)
     console.log("hola desde lo q sea");
     // console.log(data);
     // console.log(data[0].id);
-    location.reload();
+    txtid.value = "";
+    txtnom.value="";
+    cargar();
   })
   .catch(error => {
     console.error('Error al obtener datos:', error);
@@ -53,7 +121,9 @@ fetch(url)
     // console.log(data);
     // console.log(data[0].id);
     //Refrescar la página
-    location.reload();
+    txtid.value = "";
+    txtnom.value="";
+    cargar();
   })
   .catch(error => {
     console.error('Error al obtener datos:', error);
--- taller.js
+++ taller.js
@@ -6,42 +6,68 @@ const cors = require('cors'); // Importa el paquete cors
 const mysql = require('mysql2');
 const path = require('path');
 //Conectar la base de datos 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'bienestar',
-  });
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123456',
+//     database: 'bienestar',
+//   });
+const pool = mysql.createPool({
+  connectionLimit: 10, // Número máximo de conexiones simultáneas
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'bienestar',
+});
   app.use(cors()); // Habilita CORS para todas las rutas
+
+
 //usar la appi 
+// app.get('/api/data', (req, res) => {
+//     //toma el query para 
+//   let query = req.query.query; 
+//   console.log(query)
+//   let data = [];
+//     //Conecta la base de datos y realiza el query
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error('Error al conectar a la base de datos: ' + err.message);
+//     } else {
+//         executeStatement(query)
+//     }
+//   });
 app.get('/api/data', (req, res) => {
-    //toma el query para 
-  let query = req.query.query; 
+  let query = req.query.query;
   console.log(query)
   let data = [];
-    //Conecta la base de datos y realiza el query
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos: ' + err.message);
-    } else {
-        executeStatement(query)
+  pool.execute(query, (error, results) => {
+    if (error) {
+      console.error('Error al realizar la consulta:', error);
+      res.status(500).send('Error en la consulta');
+      return;
     }
+    data.push(results)
+    console.log(results)
+    res.json(results);
   });
-  //Funcion que ejecuta un query y lo guarda en la variable data
-  function executeStatement(query) {
-    connection.query(query, (error, results) => {
-        if (error) {
-          console.error('Error al realizar la consulta:', error);
-          return;
-        }else{
-            //Guarda la data de modo que se podrá usar con la api
-            data.push(results)
-            console.log(results)
-            res.json(results)
-        }
-      });
-  }
 });
+
+
+  //Funcion que ejecuta un query y lo guarda en la variable data
+  // function executeStatement(query) {
+  //   connection.query(query, (error, results) => {
+  //       if (error) {
+  //         console.error('Error al realizar la consulta:', error);
+  //         return;
+  //       }else{
+  //           //Guarda la data de modo que se podrá usar con la api
+  //           data.push(results)
+  //           console.log(results)
+  //           res.json(results)
+  //       }
+  //     });
+  // }
+
 app.use('/funciones', express.static(path.join(__dirname, 'funciones')));
 const PORT = 3001;
 //Usa la información del html
